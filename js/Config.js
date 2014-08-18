eac4e4a8e0e94fa37916ff6ac71c6432a8143578
rozifus <rozifus@gmail.com>
2014-08-18 18:25:16 +1000

    partial refactor

---
+++ js/Config.js
@@ -0,0 +1,16 @@
+
+
+var WordCloud = WordCloud || {};
+
+WordCloud.Config = function() {
+
+    this.AUTO_MOVE_TIME = 1.5;
+
+    this.USER_ZOOM_SPEED = 1.0;
+    this.USER_ROTATE_SPEED = 1.0;
+    this.USER_MOVE_SPEED = 1.0;
+
+    this.CAMERA_INIT_POSITION = [0, 150, 400];
+
+};
+
