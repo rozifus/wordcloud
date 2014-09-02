daeae86eaf83e2a00825c2e30c9fc9630fa5e957
rozifus <rozifus@gmail.com>
2014-09-03 02:30:09 +1000

    added controls text, updated css

--- js/App.js
+++ js/App.js
@@ -49,7 +49,7 @@ WordCloud.App = function (opts) {
 
     this.stats = new Stats();
     this.stats.domElement.style.position = 'absolute';
-    this.stats.domElement.style.bottom = '0px';
+    this.stats.domElement.style.top = '0px';
     this.stats.domElement.style.zIndex = 100;
     this.container.appendChild( this.stats.domElement );
 
